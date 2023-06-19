31601ed8f8e293c0b9e2fe8b8f21ba45bae9e428
Евгений Михайлюков <mihaylyukov99@gmail.com>
2023-06-19 23:04:44 +0600

    начал workerform

--- Frontend/Desktop/SteamTradeBot.Desktop.Winforms/Forms/MainForm.cs
+++ Frontend/Desktop/SteamTradeBot.Desktop.Winforms/Forms/MainForm.cs
@@ -2,17 +2,30 @@
 
 public partial class MainForm : Form
 {
-    private WorkerForm _workerForm;
+    private readonly WorkerForm _workerForm;
+    private readonly SettingsForm _settingsForm;
+
     public MainForm()
     {
         InitializeComponent();
+        _workerForm = new WorkerForm();
+        _settingsForm = new SettingsForm();
+        _settingsForm.TopLevel = false;
+        _workerForm.TopLevel = false;
+        Frame.Controls.Add(_workerForm);
+        Frame.Controls.Add(_settingsForm);
+        _workerForm.Show();
     }
 
     private void WorkerNavButton_Click(object sender, EventArgs e)
     {
-        _workerForm = new WorkerForm();
-        _workerForm.TopLevel = false;
-        Frame.Controls.Add(_workerForm);
+        _settingsForm.Hide();
         _workerForm.Show();
+    }
+
+    private void SettingsNavButton_Click(object sender, EventArgs e)
+    {
+        _settingsForm.Show();
+        _workerForm.Hide();
     }
 }
--- Frontend/Desktop/SteamTradeBot.Desktop.Winforms/Forms/WorkerForm.cs
+++ Frontend/Desktop/SteamTradeBot.Desktop.Winforms/Forms/WorkerForm.cs
@@ -1,25 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿namespace SteamTradeBot.Desktop.Winforms.Forms;
 
-namespace SteamTradeBot.Desktop.Winforms.Forms
+public partial class WorkerForm : Form
 {
-    public partial class WorkerForm : Form
+    public WorkerForm()
     {
-        public WorkerForm()
-        {
-            InitializeComponent();
-        }
-
-        private void StartButton_Click(object sender, EventArgs e)
-        {
-
-        }
+        InitializeComponent();
     }
 }
