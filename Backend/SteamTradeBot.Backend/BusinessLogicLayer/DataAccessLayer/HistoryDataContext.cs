30f49f967d10d6db888b5d5d0e38bdabd8738d23
Евгений Михайлюков <mihaylyukov99@gmail.com>
2023-06-21 22:03:24 +0600

    сделал состояние сервиса отдельным синглтоновым объектом

--- Backend/SteamTradeBot.Backend/BusinessLogicLayer/DataAccessLayer/HistoryDataContext.cs
+++ Backend/SteamTradeBot.Backend/BusinessLogicLayer/DataAccessLayer/HistoryDataContext.cs
@@ -5,7 +5,7 @@ namespace SteamTradeBot.Backend.BusinessLogicLayer.DataAccessLayer;
 
 public class HistoryDataContext : DbContext
 {
-    public DbSet<StateInfo> States { get; set; }
+    public DbSet<StateChangingEvent> States { get; set; }
 
     public HistoryDataContext(DbContextOptions<HistoryDataContext> options) : base(options)
     {
