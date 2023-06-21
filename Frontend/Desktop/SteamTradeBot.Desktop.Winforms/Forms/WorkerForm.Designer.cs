b0c6e7debdb1368fb810907a5a4a14d123394951
Евгений Михайлюков <mihaylyukov99@gmail.com>
2023-06-21 10:49:59 +0600

    +

--- Frontend/Desktop/SteamTradeBot.Desktop.Winforms/Forms/WorkerForm.Designer.cs
+++ Frontend/Desktop/SteamTradeBot.Desktop.Winforms/Forms/WorkerForm.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
             groupBox1 = new GroupBox();
             label9 = new Label();
             WarningsLabel = new Label();
@@ -283,14 +283,14 @@
             LogsDataGridView.AllowUserToDeleteRows = false;
             LogsDataGridView.BackgroundColor = Color.White;
             LogsDataGridView.BorderStyle = BorderStyle.None;
-            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = SystemColors.Control;
-            dataGridViewCellStyle1.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
-            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
-            LogsDataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = SystemColors.Control;
+            dataGridViewCellStyle2.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            LogsDataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             LogsDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             LogsDataGridView.Columns.AddRange(new DataGridViewColumn[] { Time, ItemName, OrderType, BuyPrice, SellPrice, Profit });
             LogsDataGridView.Location = new Point(6, 60);
